0cd6b3b12f1f8e2acd50704915bc4e0405ddf2fe
Sreeharsha <sreeharshak@maveric-systems.com>
2022-06-20 12:01:46 +0530

    updated deploy.sh to run docker containers, updated run-docker-compose.sh to bring down the containers before new deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 ./clone-repos.sh
 ./build-projects.sh
 ./build-images.sh
-
+./run-docker-compose.sh
--- scripts/run-docker-compose.sh
+++ scripts/run-docker-compose.sh
@@ -1,4 +1,5 @@
 mkdir -p ../code/data/mongodb/initdb.d/
 cp ./create-user.sh ../code/data/mongodb/initdb.d/
+docker-compose down
 docker network create main-network
 docker-compose --env-file "./.env" up -d 
